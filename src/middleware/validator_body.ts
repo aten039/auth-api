9814dde140a3291ed7a19a7fd289c4e6bcab1817
Elvis Linares <elvis.039@hotmail.com>
2025-01-22 00:28:20 -0400

    create middleware for validate body

---
+++ src/middleware/validator_body.ts
@@ -0,0 +1,34 @@
+import {Request , Response, NextFunction} from "express"
+
+export  function validate_body( property:string, type:string) {
+
+    return ( req:Request, res:Response, next:NextFunction) => {
+        try {
+            if (!req.body.hasOwnProperty(property)){
+                return res.status(400).json({error:{
+                    message: `La propiedad ${property} es obligatoria`,
+                    property: property,
+                    error:true
+                }})
+            }
+    
+            if( typeof req.body[property] != type ){
+                return res.status(400).json({error:{
+                    message: `La propiedad ${property} tiene un valor incorrecto`,
+                    property: property,
+                    error:true
+                }})
+            }
+    
+            next()
+
+        } catch (error) {
+            return res.status(500).json({
+                message: `Ha ocurrido un error al procesar su solicitud`,
+                error:true
+            })
+        }
+        
+    }
+
+}
