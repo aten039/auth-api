d6c93d94da00a947e0b541cb082629b28d581dd6
Elvis Linares <elvis.039@hotmail.com>
2025-02-03 18:31:21 -0400

    cors

---
+++ src/config/cors.ts
@@ -0,0 +1,19 @@
+import { CorsOptions } from "cors";
+
+export const corsConfig: CorsOptions = {
+    origin:function(requestOrigin, callback) {
+       
+        const whiteList = [process.env.FRONTEND_URL]
+
+        const api = process.argv[2]
+        if(api === '--api'){
+            whiteList.push(undefined)
+        }
+        
+        if(whiteList.includes(requestOrigin)){
+            callback(null, true);
+        }else{
+            callback(new Error('errors CORS'));
+        }
+    },
+}
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import express  from "express";
 import authRouter from "./routes/authRouter";
 import 'dotenv/config'
 import { connectDb } from "./config/db";
+import { corsConfig } from "./config/cors";
+import cors from "cors"
 
 connectDb()
 const app = express()
+app.use(cors(corsConfig));
 app.use(express.json())
 
 app.use('/auth', authRouter)
